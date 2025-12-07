300d99e77b3bfd7d5f437a5d3dd1ae2c1eb5579d
radelqui <130411099+radelqui@users.noreply.github.com>
2025-12-07 15:22:51 -0500

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'menu-fusion-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700;800&display=swap'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
